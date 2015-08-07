ebabc64b2643a2e0b9e9f7d9fb28bbfb1c9281e4
taoran <taoran@sensetime.com>
2015-08-07 18:39:39 +0800

    Working progress...

--- CameraLearnSwift/CameraLearnSwift/HideEyeCameraController.swift
+++ CameraLearnSwift/CameraLearnSwift/HideEyeCameraController.swift
@@ -7,4 +7,27 @@
 //
 
 import Foundation
+import UIKit
+import AVFoundation
 
+protocol HideEyeCameraDelegate : NSObjectProtocol{
+//    optional func getPic();
+}
+
+class HideEyeCameraController: NSObject, AVCaptureAudioDataOutputSampleBufferDelegate {
+    var ownerController : UIViewController
+    weak var cameraDelegate : HideEyeCameraDelegate?
+    var captureSession : AVCaptureSession?
+    private var videoCaputureInput : AVCaptureDeviceInput?
+    private var movieFileOutput : AVCaptureMovieFileOutput?
+    private var stillImageOutput : AVCaptureStillImageOutput?
+    private var sessionQueue : dispatch_queue_t?
+    init(cameraOwner : UIViewController){
+        self.ownerController = cameraOwner;
+        super.init()
+//        self.cameraDelegate = cameraOwner;
+        self.captureSession = AVCaptureSession();
+        self.sessionQueue = dispatch_queue_create("session queue", DISPATCH_QUEUE_SERIAL);
+        
+    }
+}
